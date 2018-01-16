68fcd2f951b3b956debe7720fec91d82b1fdbc17
JWS <jacob.steubing@gmail.com>
2018-01-16 08:09:34 -0800

    Filling out the metering function

--- meter.sol
+++ meter.sol
@@ -1,8 +1,33 @@
 pragma solidity ^0.4.0;
 
 
-contract meter {
-    function meter(){
+contract Meter is Ownable {
 
+    struct Meter {
+        bool export; ///determines if meter is net load or generation
+        bool powerFlow; ///net power flow in or out of the meter
     }
+
+    struct Meter meter;
+
+    function setMeter(bool _export, uint _powerFlow) private {
+        meter.export = _export;
+        meter.powerFlow = _powerFlow;
+    } ///private function set by the actual account meter
+
+}
+
+contract genMeter is meter{
+    ///create and offer structure
+    ///make offers
+    ///
+}
+
+contract loadMeter is meter {
+
+}
+
+contract storageMeter is meter {
+
+
 }
