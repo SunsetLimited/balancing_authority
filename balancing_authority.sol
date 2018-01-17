eedcb43a10920afa84e645a57abc3d3852fcd701
JWS <jacob.steubing@gmail.com>
2018-01-16 22:25:39 -0800

    filled out the meter functions

--- balancing_authority.sol
+++ balancing_authority.sol
@@ -1,25 +1,39 @@
 pragma solidity ^0.4.0;
 
+import "./meter.sol";
 
-contract balancingAuthority is Ownable {
-    //receive meter data
+contract balancingAuthority {
+    //the first function of the balancing authority account is to maintain the microgrid's metered state vis-a-vis the
+    //larger electric power distribution system
     struct SystemMeter {
-        bool export; ///indicates if the local balacning authority is net importing or exporting with larger electric system
+        bool export; ///indicates if the local balancing authority is net importing or exporting with distribution system
         uint powerFlow; ///indicates the net inflow/outflow (determined by status of export variable)
         uint systemBid; ///spot price the distribution system pays for exports, e.g.; time-of-use net metered rate
         uint systemOffer; ///spot price the distribution system charges for imports
     }
 
-    SystemMeter systemMeter; ///define the actual system meter
+    SystemMeter systemMeter; ///declare the system meter
 
     function setSystemMeter(bool _export, uint _powerFlow, uint _systemBid, uint _systemOffer) private{
         systemMeter.export = _export;
         systemMeter.powerFlow = _powerFlow;
         systemMeter.systemBid = _systemBid;
         systemMeter.systemOffer = _systemOffer;
-    } ///private function called by the balancing authority account owner, reads the physical meter
+    } ///private function called by the balancing authority account owner, reads the physical meter at the point of
+        ///interconnection to the larger system
 
+    ///the second function of the balancing authority account is to facilitate the interaction of all metered accounts
 
+    mapping (uint => address) meters; ///all metered accounts
+    mapping (uint => address) genMeters; ///all metered accounts with associated distributed generation
+///###NEED A FUNCTION TO ADD NEW ACCOUNTS###
+    struct MarketDay {
+        uint year;
+        uint month;
+        uint day;
+        }
+
+    ///storage meters are included in both mappings, as the can perform the function of load and generation
 
     //system balancing
         //read offers from meters
@@ -31,6 +45,9 @@ contract balancingAuthority is Ownable {
 
     //balancing function
         //read data from metered accounts
+    //real-time dispatch and true-up functions
+        ///saved day-ahead offers, check against meter for each account,
+
     //islanding function
 
 
@@ -38,3 +55,6 @@ contract balancingAuthority is Ownable {
     }
 }
 
+contract SystemMeter is GenMeter{
+    ///the system meter is a gen meter that can island
+}
--- meter.sol
+++ meter.sol
@@ -1,33 +1,64 @@
 pragma solidity ^0.4.0;
 
-
-contract Meter is Ownable {
+///meters are associated with accounts
+contract MeteredAccount {
 
     struct Meter {
         bool export; ///determines if meter is net load or generation
-        bool powerFlow; ///net power flow in or out of the meter
+        uint powerFlow; ///net power flow in or out of the meter, determined by export variable
     }
 
-    struct Meter meter;
+    Meter meter; ///declaring the meter for the account
 
-    function setMeter(bool _export, uint _powerFlow) private {
+    function setMeter(bool _export, uint _powerFlow) internal {
         meter.export = _export;
         meter.powerFlow = _powerFlow;
-    } ///private function set by the actual account meter
+                    } ///function set by the actual account meter
+
+    struct MarketDay {
+        uint year;
+        uint month;
+        uint day;
+        } ///designation of the market day for which the offers are made
+
+    struct DayAheadBid {
+        uint[] quantities;///array of hourly load forecast, hour-beginning convention for indexing
+        MarketDay day; ///the day for which the offers are made
+        }///define the datatype for a day ahead bid to purchase power from the metered account
+            ///generally, load accounts are price takers, though they may choose to curtail in the real-time
+
+    DayAheadBid dayAheadBid;///declare the day ahead bid variable
+
+    function _setDayAheadBid(uint[], _quantities, uint[] _prices, MarketDay _day) internal{
+        dayAheadBid.quantities = _quantities;
+        dayAheadBid.prices = _prices;
+        dayAhead.dayAhead = _day;
+    }///function to make a bid that becomes readable by the balancing authority
+
+    function readDayAheadBid external returns(DayAheadBid){
+        return dayAheadBid;
+    }///external function to allow the balancing authority to read the day-ahead bid from the load account
+
 
 }
 
-contract genMeter is meter{
-    ///create and offer structure
-    ///make offers
-    ///
+contract GenMeter is MeteredAccount {
+
+    struct DayAheadOffer {
+        uint[] quantities;///array of hourly generation quantities offered, hour-beginning convention for indexing
+        uint[] prices;  ///array of offered prices corresponding to the 'quantities' array
+        MarketDay day; ///the day for which the offers are made
+        }///defines the datatype for a day ahead offer to sell power from the metered account
+
+    DayAheadOffer dayAheadOffer; ///declares the day-ahead offer variable
+
+    function _setDayAheadOffer(uint[], _quantities, uint[] _prices, MarketDay _day) internal{
+        dayAheadOffer.quantities = _quantities;
+        dayAheadOffer.prices = _prices;
+        dayAheadOffer.day = _day;
+    }///function to make an offer that becomes readable by the balancing authority
+
+    ///GenMeter can make load offers
+
 }
 
-contract loadMeter is meter {
-
-}
-
-contract storageMeter is meter {
-
-
-}
