aa2aa8cd3df2a6b4ab4fc4e708ba6a8a60820bff
JWS <jacob.steubing@gmail.com>
2018-01-24 08:03:14 -0800

    cleaning it up for final publishing

--- balancing_authority.sol
+++ balancing_authority.sol
@@ -12,7 +12,7 @@ contract SystemMeter is GenMeter{
 
 
 
-///VERIFIED IN REMIX
+///0: ESTABLISH INTERFACES FOR BALANCING AUTHORITY TO DRAW DATA FROM METERS
 contract MeterInterface {
 function readMeter() external view returns(uint);
 }
@@ -48,8 +48,10 @@ contract HourAheadOfferInterface {
         uint hour);
 }
 
+/// BALANCING AUTHORITY CONTRACT
 contract BalancingAuthority{
-
+   ///FOUNDATIONAL INTERFACES AND VARIABLES FOR INTERACTING WITH THE METERED POWER SYSTEM
+    ///1(a): define variables and associated functions to interact with the above-defined interfaces
     mapping (uint => address) meterMap;
     mapping (uint => address) genMeterMap;
 
@@ -67,7 +69,7 @@ contract BalancingAuthority{
     uint genMeterCount = 0;
 
 
-    function addMeter(address _address)  {
+    function addMeter(address _address)  { //core function to admit new meters to the balancing authority
          uint _maxLoad;
          bool _gen;
          uint _nameplate;
@@ -77,13 +79,13 @@ contract BalancingAuthority{
        uint _id = systemAccounts.push(MeteredAccount(_address, _maxLoad, _gen, _nameplate, _storageCapacity)) -1;
        meterMap[_id] = _address;
        meterCount++;
-       if(_gen == true){
+       if(_gen == true){ //setting a separate mapping of generation accounts for use in reading offers
            genMeterMap[genMeterCount] = _address;
            genMeterCount++;
        }
 
     }
-
+//  1(b): functions to pull descriptive data about the current (instantaneous) state of the balancing authority system
     function getMeterCount() returns(uint){
         return meterCount;
     }   //returns total count of meters on the system
@@ -97,7 +99,6 @@ contract BalancingAuthority{
         return meterReader.readMeter();
     } //returns the current load of a given meter
 
-
     function getSystemLoad() returns(uint) {
        uint _cumulativeLoad = 0;
        for(uint i = 0; i < meterCount; i++){
@@ -106,59 +107,52 @@ contract BalancingAuthority{
        return _cumulativeLoad;
     }
 
-    function _readHourAheadBid(address _address) returns(uint, uint, uint, uint, uint){
-        HourAheadBidInterface hourAheadBids = DayAheadBidInterface(_address);
-        return hourAheadBids.hourDayAheadBid();
-    }
 
-    struct systemDayAheadLoad{
-      uint year;
-      uint month;
-      uint day;
-      uint[3] dayAheadLoad; ///hourly expected load
-    }//structure to record the system-wide day ahead load
+    ///Gen meter interaction functions
 
-    function getDayAheadBids(uint _year, uint _month, uint _day) returns(uint[3]){
-        uint[3] memory _cumulativeQuantities;
-        for(uint i = 0; i < meterCount; i++){
-            uint _bidYear;
-            uint _bidMonth;
-            uint _bidDay;
-            (, _bidYear, _bidMonth, _bidDay) = _readDayAheadBid(systemAccounts[i].accountAddress);
-            if(_bidYear == _year && _bidMonth == _month && _bidDay == _day){
-                uint[3] memory _bidQuantities;
-                (_bidQuantities,,,) = _readDayAheadBid(systemAccounts[i].accountAddress);
-                for(uint j = 0; j < 3; j++){
-                    _cumulativeQuantities[j] += _bidQuantities[j];
-                }
-            }
-        }
-        return _cumulativeQuantities;
-    }
-
-    function _readGen(address _address) returns(uint){
+    function _readGen(address _address) returns(uint){ // reads instantaneous generation from a given meter
         GenMeterInterface genReader = GenMeterInterface(_address);
         return genReader.readGen();
     }
 
-    function _getSystemGen() returns(uint){
+    function _getSystemGen() returns(uint){//returns the instantaneous generation within the control area
         uint _cumulativeGen = 0;
         for (uint i = 0; i < genMeterCount; i++){
             _cumulativeGen += _readGen(genMeterMap[i]);
         }
         return _cumulativeGen;
     }
+  //2: MARKET CLEARING FUNCTIONS
+    function _readHourAheadBid(address _address) returns(uint, uint, uint, uint, uint){
+        HourAheadBidInterface hourAheadBids = DayAheadBidInterface(_address);
+        return hourAheadBids.hourDayAheadBid();
+    }
 
-    function _readHourAheadOffer(address _address) returns (uint, uint, uint, uint, uint, uint){
+    function getDayAheadBids(uint _year, uint _month, uint _day, uint _hour) returns(uint){
+        uint memory _cumulativeBids;
+        for(uint i = 0; i < meterCount; i++){
+            uint _bidQuantity;
+            uint _bidYear;
+            uint _bidMonth;
+            uint _bidDay;
+            uint _bidHour;
+            (_bidQuantity, _bidYear, _bidMonth, _bidDay, _bidHour) = _readDayAheadBid(systemAccounts[i].accountAddress);
+            if(_bidYear == _year && _bidMonth == _month && _bidDay == _day && bidHour == _hour){
+                    _cumulativeBids += _bidQuantity;
+                }
+                                    }
+        return _cumulativeQuantity;
+    }
+    function _readHourAheadOffer(address _address) returns (uint, uint, uint, uint, uint, uint){//reads hour-ahead offer from a given meter
         HourAheadOfferInterface hourAheadOffer = HourAheadOfferInterface(_address);
         return hourAheadOffer.readHourAheadOffer();
     }
 
-
-function _getLowestAboveX(uint[] _array, uint _x) returns(uint){ //function to get index of lowest value in an array, above zero
+///
+function _getLowestAboveX(uint[] _array, uint _x) returns(uint){ //for itering through to find marginal prices
     uint _lowestIndex = 0;
-    while(_array[_lowestIndex] <= _x){
-        _lowestIndex++;
+    while(_array[_lowestIndex] =< _x){ //first iterate through array until finding next higher price
+        _lowestIndex++; //bug in this prototype: handling identical offers, and offers == 0;
     }
     for(uint i = _lowestIndex; i < _array.length; i++){
         if(_array[i] > _x){
@@ -171,8 +165,11 @@ function _getLowestAboveX(uint[] _array, uint _x) returns(uint){ //function to g
 }
 
     ////////////////Pick up here
+    //clear hourly marginal prices
+    //get
     uint[3] _hourlyLoad;
-    function _setHourlyLoad(uint _year, uint _month, uint _day){
+
+    function _getHourlyLoad(uint _year, uint _month, uint _day){
         _hourlyLoad = getDayAheadBids(_year, _month, _day);
     }
 
--- meter.sol
+++ meter.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.0;
 
 contract Meter{
+ ///0: DEFINE BASIC VARIABLES AND FUNCTIONS OF A LOAD-ONLY METER
+    ///0(a): Setting load and defining meter specs
+
     uint load; //current power usage
     uint maxLoad; //(kW)maximum capacity of the meter
     bool gen = false; //indicates whether or not the meter has associated generation
@@ -10,18 +13,21 @@ contract Meter{
         maxLoad = _maxLoad;
     } //meter owner sets its maximum power usage
 
-    function getMeterSpecs() public view returns(uint, bool, uint, uint){
-        return (maxLoad, gen, 0, 0);
-    } //allows Balancing Authority to call information on the load meter; returns 0 for generation specs
-
     function setLoad(uint _load) {
         load = _load;
     } //function for the physical meter to record instantaneous power usage
+
+  ///0(b) functions for reading basic load data from the meter
+    function getMeterSpecs() public view returns(uint, bool, uint, uint){
+        return (maxLoad, gen, 0, 0);
+    } //allows Balancing Authority to call information on the load meter; returns 0 for generation specs
 
     function readMeter() public view returns(uint){
         return(load);
     } //function for balancing authority or other external contracts to read the current load
 
+    ///1: MARKET FUNCTIONS
+        //1(a)setting hour-ahead bids
     struct HourAheadBid {
         uint quantity;///array of hourly load forecast, hour-beginning convention for indexing
         uint year;
@@ -41,13 +47,16 @@ contract Meter{
         hourAheadBid.hour - _hour;
     }///function to make a bid that becomes readable by the balancing authority
 
+    //1(b) allowing balancing authority to read the hour-ahead bid
+
     function readHourAheadBid() external returns(uint, uint, uint, uint, uint){
         return (hourAheadBid.quantity, hourAheadBid.year, hourAheadBid.month, hourAheadBid.day, hourAheadBid.hour);
     }///external function to allow the balancing authority to read the day-ahead bid from the load account
 
 }
-
+///2: GENERATION METER
 contract GenMeter is Meter{
+    //2(a) defining the basic variables and inputfunctions of a generation meter
     uint generation; //the instantaneous generation, set by the physical meter
     bool gen = true;
     uint nameplate; //(kW)nameplate capacity of the generation associated with the meter
@@ -60,19 +69,21 @@ contract GenMeter is Meter{
         storageCapacity = _storageCapacity;
     }
 
-    function getMeterSpecs() public view returns(uint, bool, uint, uint){
-        return (maxLoad, gen, nameplate, storageCapacity);
-    }
-
     function setGeneration(uint _generation) {
         generation = _generation;
     } //function for the physical meter to record instantaneous power usage
 
+    //2(b) functions to allow balancing authority to read meter data
+    function getMeterSpecs() public view returns(uint, bool, uint, uint){
+        return (maxLoad, gen, nameplate, storageCapacity);
+    }
 
     function readGen() public view returns(uint){
         return(generation);
     } //function for balancing authority or other external contracts to read the current load
 
+    //3: GENERATION MARKET ACTIVITIES
+    //3(a) functions to interact with the balancing authority and make offers into the day-ahead market
     struct HourAheadOffer {
         uint price; //($/kWh) price at which the generation is offered
         uint quantity;//(kWh)array of hourly generation offer, hour-beginning convention for indexing
@@ -91,7 +102,9 @@ contract GenMeter is Meter{
         hourAheadOffer.month = _month;
         hourAheadOffer.day = _day;
         hourAheadOffer.hour = _hour;
-    }///function to make an offer that becomes readable by the balancing authority
+    }
+
+    //3(b) Function to allow balancing authority to read offer data
 
     function readHourAheadOffer() external returns(uint, uint, uint, uint, uint, uint){
         return (hourAheadOffer.price, hourAheadOffer.quantity, hourAheadOffer.year, hourAheadOffer.month, hourAheadOffer.day, hourAheadOffer.hour);
