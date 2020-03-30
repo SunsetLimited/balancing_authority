84702958fc4432ecac685166bfee7ab4c6b7fc38
jacob <jacob.steubing@gmail.com>
2020-03-29 23:19:55 -0400

    further fleshing out the meter objects for generation

--- market.py
+++ market.py
@@ -1,5 +1,20 @@
 #using the objectins in meters.py, this creates an auction simulation
+#Purpose: determine battery cost necessary for autuarky given market conditions
 
 #input: ecosystem of meters
+    #create meters (loop through, function should generate meters and capacity within random parameters)
     #initiating bids and offers (bids from load meters, offers from gen meters)
-    #
+        #offerss will be set at the LCOE
+        #wholesale prices will be based on EIA forecasts
+        #gen shape from the EIA
+        #battery efficiency can come from the Lazard, EIA, etc
+    #use affiliances to track sub-markets
+#operations: balancing, creating clearing price
+    #sort bids and offers within relevant markets
+    #create clearing price, update ledger to reflect payments/receipts
+        #ledger: separate object, likely within a dataframe (reflect .sol potentiality)
+    #benchmark clearing price within a market against the index of pure wholesale prices
+#output
+    #total systemwide savings/loss vs index, over time
+    #net position of gen/load vs LCOE
+    #report output by autark-affiliance, and by gen project
--- meters.py
+++ meters.py
@@ -1,23 +1,33 @@
 ##creating the meters objects in python in anticipating of a market simulation
 
 class meter(): #foundational object, each other type will branch from this
-    def __init__(self, capacity):
+    def __init__(self, capacity): #most basic data for a meter: max capacity for kW flow
         self.capacity = capacity
         self.bidPrice = 0.0
         self.bidQuantity = capacity
-    def bid(self, bidPrice, bidQuantity):
+    def bid(self, bidPrice, bidQuantity):#all meters can purchase power
         self.bidPrice = bidPrice
         if bidQuantity > self.capacity:
             self.bidQuantity = self.capacity ##obviously, can't buy beyond meter capacity
         else:
             self.bidQuantity = bidQuantity
 
+
 class genMeter(meter):
-    def _init_(self, capacity, technology):
+    def __init__(self, capacity, technology, genCapacity): #defining the generation behind the meter
         self.technology = technology
-        meter.__init__(self, name)
+        meter.__init__(self, capacity)
+        if genCapacity > self.capacity:
+            self.genCapacity = capacity
+        else:
+            self.genCapacity = genCapacity
+    def offer(self, offerPrice, offerQuantity):
+        self.offerPrice = offerPrice
+        if offerQuantity > self.genCapacity:
+            self.offerQuantity = genCapacity
+        else:
+            self.offerQuantity = offerQuantity
 
-
-
-class generation():
-    def _init_(self, capacity,  technology, ):
+#create class for storage meter as well
+    #capacity in hrs, plus efficiency
+    #assume constrain in charge =  discharge
